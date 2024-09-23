1db71e902ae52b8f7608f3e9f26987afb28734a6
Peter.c <peteropuru1@gmail.com>
2024-09-23 12:18:16 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <math.h>
 int main()
 {
-    printf("Hello world!\n");
+    float principle, rate, time, amount, compoundinterest;
+    printf("Enter the principle");
+    scanf("%f",&principle);
+
+    printf("Enter the time");
+    scanf("%f",&time);
+
+    printf("Enter the rate");
+    scanf("%f",&rate);
+
+    amount = principle * pow((1+(rate/100)), time);
+    compoundinterest=amount-principle;
+
+    printf("The compoundinterest is %.2f \n",compoundinterest);
     return 0;
 }
